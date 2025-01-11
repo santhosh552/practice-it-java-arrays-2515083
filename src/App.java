f0f06e46bdd7c12b1b9e5f760cf404830e700336
santhosh552 <santhosh552@gmail.com>
2025-01-11 21:06:38 +0000

    Implement display traingle

--- src/App.java
+++ src/App.java
@@ -1,14 +1,21 @@
 public class App {
-  
+
   public static void printTriangle(Object[] arr) {
 
- }
-    
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j <= i; j++) {
+        System.out.print(arr[j]);
+      }
+      System.out.println();
+    }
+    System.out.println();
+  }
+
   public static void main(String args[]) {
-    printTriangle(new Integer[] {1, 2, 3, 4});
-    printTriangle(new Character[] {'A', 'B', 'C', 'D', 'E'});
+    printTriangle(new Integer[] { 1, 2, 3, 4 });
+    printTriangle(new Character[] { 'A', 'B', 'C', 'D', 'E' });
     printTriangle(new String[] {});
-    printTriangle(new String[] {"Hello"});
-    printTriangle(new String[] {"Hello", "There", "Friend"});
+    printTriangle(new String[] { "Hello" });
+    printTriangle(new String[] { "Hello", "There", "Friend" });
   }
 }

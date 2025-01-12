62c8571ccc718345be967edf8c42c02df9c6674d
santhosh552 <santhosh552@gmail.com>
2025-01-12 02:33:20 +0000

    implement moving zeros to end in arrays

--- src/App.java
+++ src/App.java
@@ -3,6 +3,20 @@ import java.util.Arrays;
 public class App {
 
   public static void moveZerosToTheEnd(int[] arr) {
+    int count=0;
+    for (int i=0;i < arr.length; i++)
+    {
+      if (arr[i] != 0)
+      {
+        arr[count] = arr[i];
+        count++;
+      }
+    }
+    while(count < arr.length)
+    {
+      arr[count] = 0;
+      count++;
+    }
     
   }
 
